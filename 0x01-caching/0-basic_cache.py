edcf0b7ebd837b2b0633e9ee3e1fef43dcf662a9
stewie-gil <gilliganstewie7@gmail.com>
2023-08-22 04:47:08 -0700

    basic cache

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Module contains the BasicCache that inherits from BaseCaching
+"""
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """
+    BasicCache inherits fro BaseCaching
+    """
+
+    def __init__(self):
+        """ initializing base_caching"""
+        super().__init__()
+
+    def put(self, key, item):
+        """
+        Stores key value in self.cache_data
+        Args: key - key for self.cache_data dict
+        item- value for the key
+        """
+        if key is not None or item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ Retrieves items based on key
+        Args: key - key used for retrieval
+        return item based on the key
+        """
+        if key is None:
+            return None
+        elif key not in self.cache_data:
+            return None
+        return self.cache_data[key]
---
+++ 0x01-caching/README.md
@@ -0,0 +1,3 @@
+0x01. Caching
+______________
+Practicing caching with python
