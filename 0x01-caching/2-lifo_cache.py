6da6b434b2c12fbd8410f7867a73f610531cbef3
stewie-gil <gilliganstewie7@gmail.com>
2023-08-23 05:47:25 -0700

    minor fixx on lifo

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -21,11 +21,11 @@ class LIFOCache(BaseCaching):
         """
         deletes last item put in cache
         """
-        self.cache_data[key] = item
-        if key in self.key:
-            self.key.remove(key)
-        self.key.append(key)
-        if key or item is not None:
+        if key and item:
+            self.cache_data[key] = item
+            if key in self.key:
+                self.key.remove(key)
+            self.key.append(key)
             if len(self.cache_data) > self.MAX_ITEMS:
                 to_delete = self.key[-2]
                 print("DISCARD: {}".format(to_delete))
