d37f0bf7f689ef3af5792bb93767c4d0b9266a9f
stewie-gil <gilliganstewie7@gmail.com>
2023-08-23 05:43:09 -0700

    fixed documentation

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -8,8 +8,11 @@ from base_caching import BaseCaching
 
 
 class LRUCache(BaseCaching):
+    """ the LRUCache class inherits
+    from BaseCaching"""
+
     def __init__(self):
-        """ Init function
+        """ Init function for the LRUCache.
         """
         super().__init__()
         self.key = []
@@ -18,15 +21,16 @@ class LRUCache(BaseCaching):
         """ put function
         Args: key and item  to add to the dict
         """
-        if key in self.key:
-            self.key.remove(key)
-        self.key.append(key)
-
-        self.cache_data[key] = item
         if key and item:
+
+            if key in self.key:
+                self.key.remove(ke)
+            self.key.append(key)
+
+            self.cache_data[key] = item
             if len(self.cache_data) > self.MAX_ITEMS:
                 to_discard = self.key[0]
-                print("DISCARD: ", to_discard)
+                print("DISCARD: {}".format(to_discard))
                 del self.cache_data[to_discard]
                 self.key.remove(to_discard)
 
