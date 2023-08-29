089854125d941129fbd5e510d0aaef6d45ff793b
stewie-gil <gilliganstewie7@gmail.com>
2023-08-29 11:56:04 -0700

    babel translations enabled

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""Basic flask application"""
+
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """A config class with default
+    local and timezone
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """using request.accept_languages to determine the best
+    match with supported languages
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route("/")
+def index():
+    """ serves 2-index.html file for route /"""
+    return render_template("templates/2-index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
