cda840a722df3c83b194ff408807388c2610efe4
stewie-gil <gilliganstewie7@gmail.com>
2023-08-29 10:22:45 -0700

    fix typo

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -22,7 +22,7 @@ class Config:
 app.config.from_object(Config)
 
 
-@babel.localselector
+@babel.localeselector
 def get_locale():
     """using request.accept_languages to determine the best
     match with supported languages
