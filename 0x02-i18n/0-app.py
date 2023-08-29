49ba8fcc6a8cecc818315955de9a46aad2d1f07b
stewie-gil <gilliganstewie7@gmail.com>
2023-08-29 08:43:12 -0700

    fixing black spaces

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -13,5 +13,6 @@ def index():
     """ serves 0-index.html file for route /"""
     return render_template("0-index.html")
 
+
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run(debug=True)
