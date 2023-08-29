b272dd6b6ed8b5d1e0878c6689405e4f0ff0512d
stewie-gil <gilliganstewie7@gmail.com>
2023-08-29 12:04:01 -0700

    typo

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""Basic flask application"""
+
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """A config class with default
+    local and timezone
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """using request.accept_languages to determine the best
+    match with supported languages
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route("/")
+def index():
+    """ renders index.html with the new content"""
+    return render_template(
+        "templates/3-index.html", title=_("home_title"), header=_("home_header"))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
